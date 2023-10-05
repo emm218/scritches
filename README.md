2bc3f41532084a06d2a36ed956630effbe3a9b66
Emmy <32604310+emm218@users.noreply.github.com>
2023-10-05 15:03:00 -0400

    added proper config handling

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 I have some problems with existing solutions for scrobbling my music to last.fm
 with MPD so I'm trying to do a rewrite
+
+## Configuration
+
+The config file is stored in `$XDG_CONFIG_HOME/scritches` by default and can use
+any of the file formats supported by the
+[config](https://crates.io/crates/config) crate (YAML, TOML, etc.) as long as
+the part before the extension is `config`. The MPD hostname and port can also be
+read from the `MPD_HOST` and `MPD_PORT` environment variables which will 
+override the settings in the config file. Command line option have the highest
+priority, overriding both environment variables and config files.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use config::Config;
+use serde_derive::Deserialize;
+
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[command(version)]
+struct Args {
+    /// Path to config file
+    #[arg(short, long)]
+    config: Option<PathBuf>,
+
+    /// detach from current terminal after startup
+    #[arg(short, long)]
+    detach: bool,
+
+    /// hostname for MPD
+    #[arg(short = 'H', long)]
+    host: Option<String>,
+
+    /// port for MPD
+    #[arg(short, long)]
+    port: Option<u16>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Settings {
+    mpd_host: String,
+    mpd_port: u16,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let mut config_builder = Config::builder()
+        .set_default("mpd_host", "localhost")?
+        .set_default("mpd_port", 6600)?;
+
+    if let Some(host) = args.host {
+        config_builder = config_builder.set_override("mpd_host", host)?;
+    }
+
+    if let Some(port) = args.port {
+        config_builder = config_builder.set_override("mpd_port", port)?;
+    }
+
+    if let Some(config_path) = args.config {
+        config_builder = config_builder.add_source(config::File::with_name(
+            config_path.to_str().expect("invalid UTF-8 in config path"),
+        ));
+    } else {
+        config_builder = config_builder.add_source(
+            config::File::with_name(
+                xdg::BaseDirectories::with_prefix("scritches")?
+                    .get_config_file("config")
+                    .to_str()
+                    .expect("invalid UTF-8 in config path"),
+            )
+            .required(false),
+        );
+    }
+
+    config_builder = config_builder.add_source(config::Environment::default());
+
+    let settings: Settings = config_builder.build()?.try_deserialize()?;
+
+    println!("{}:{}", settings.mpd_host, settings.mpd_port);
+
+    Ok(())
 }
