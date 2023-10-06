fa7c8c05826281df8bfbb8836058ccd1a917222c
Emmy <32604310+emm218@users.noreply.github.com>
2023-10-06 08:38:53 -0400

    add logic for determining when to scrobble
    just prints out ID of song to be scrobbled right now but properly
    handles repeating songs and scrobbling threshold

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#![feature(let_chains)]
+#![feature(duration_constants)]
+
 use clap::Parser;
-
-use std::path::PathBuf;
-
 use mpd::idle::{Idle, Subsystem};
+
+use std::cmp::min;
+use std::time::Duration;
+use std::path::PathBuf;
 
 mod config;
 
@@ -33,9 +37,59 @@ fn main() -> anyhow::Result<()> {
 
     let mut conn = mpd::Client::connect(&format!("{}:{}", settings.mpd_host, settings.mpd_port))?;
 
-    loop {
-        println!("Status: {:?}", conn.status());
+    let stats = conn.stats()?;
+    let status = conn.status()?;
 
-        conn.wait(&[Subsystem::Player])?;
-    }
+    let elapsed = status.elapsed.unwrap_or_default();
+    let length = status.duration.unwrap_or_default();
+
+    conn.wait(&[Subsystem::Player])?;
+
+    next_event(
+        stats.playtime - elapsed,
+        length,
+        elapsed,
+        status.song.map(|s| s.id),
+        &mut conn,
+    )
+}
+
+fn next_event(
+    prev_start: Duration,
+    prev_length: Duration,
+    prev_elapsed: Duration,
+    prev_song: Option<mpd::song::Id>,
+    conn: &mut mpd::Client,
+) -> anyhow::Result<()> {
+    let status = conn.status()?;
+    let cur_time = conn.stats()?.playtime;
+
+    let elapsed = status.elapsed.unwrap_or(prev_elapsed);
+
+    let (start, length, song) = match (prev_song, status.song.map(|s| s.id).zip(status.duration)) {
+        (Some(id), Some((id2, _))) if id == id2 => {
+            let t = if check_submit(prev_start, cur_time, prev_length) && elapsed < Duration::from_secs(1) {
+                println!("{id}");
+                cur_time
+            } else {
+                prev_start
+            };
+            (t, prev_length, prev_song)
+        }
+
+        (old, new) => {
+            if check_submit(prev_start, cur_time, prev_length) && let Some(id) = old { 
+                println!("{id}");
+            }
+            (cur_time, new.map_or(prev_length, |s| s.1), new.map(|s| s.0))
+        }
+    };
+
+    conn.wait(&[Subsystem::Player])?;
+    next_event(start, length, elapsed, song, conn)
+}
+
+#[inline]
+fn check_submit(start: Duration, cur: Duration, length: Duration) -> bool {
+    (cur - start) >= min(Duration::from_secs(240), length / 2) 
 }
