c766ceb81321a85a7dafcde389a5a0c6a350131a
Emmy <32604310+emm218@users.noreply.github.com>
2023-10-17 00:22:33 -0400

    add types for interaction with last.fm api

---
+++ src/last_fm.rs
@@ -0,0 +1,32 @@
+use serde_derive::{Deserialize, Serialize};
+
+use std::time::SystemTime;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ScrobbleInfo {
+    pub title: String,
+    pub artist: String,
+    pub album: Option<String>,
+    pub album_artist: Option<String>,
+    pub track_id: Option<String>,
+    pub start_time: SystemTime,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BasicInfo {
+    pub title: String,
+    pub artist: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Message {
+    Scrobble(ScrobbleInfo),
+    NowPlaying(BasicInfo),
+    Action(Action),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Action {
+    LoveTrack(BasicInfo),
+    UnloveTrack(BasicInfo),
+}
